5849a1dbea2ac064e5591cebc8218c11c7604c16
LucianoKuhn <87299983+LucianoKuhn@users.noreply.github.com>
2022-02-08 02:32:49 -0300

    Add files via upload

---
+++ pages/BlogJS.js
@@ -0,0 +1,3 @@
+document.form.text_area.value ='\n           -Mitsubishi Lancer GLS- \n\n  ▪Año:  2016 \n  ▪Motor:  2.0L,  154HP \n  ▪Carroceria:   Sedan \n  ▪Fecha: 25/01/2022 \n ▪Valor Aprox: 23.000 USD';
+
+document.form2.text_area2.value ='\n               -Ford F-150 Raptor- \n\n  ▪Año:  2021 \n  ▪Motor:  3.5L Bi-turbo, 456HP \n  ▪Carroceria:   Pick-Up \n  ▪Fecha: 07/02/2022 \n ▪Valor Aprox: 170.000 USD';
