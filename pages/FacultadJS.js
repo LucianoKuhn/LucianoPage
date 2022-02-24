0a20fdab6fcb082fca17cebb105f80fc53f05b11
LucianoKuhn <87299983+LucianoKuhn@users.noreply.github.com>
2022-02-23 22:21:58 -0300

    Add files via upload

--- pages/FacultadJS.js
+++ pages/FacultadJS.js
@@ -3,13 +3,15 @@ var fecha = hoy.toDateString();
 var hora = hoy.getHours() + ':' + hoy.getMinutes() + ':' + hoy.getSeconds();
 var diaSemana = fecha.substring(0,3);
 
-//document.querySelector('.grid-item:nth-child(2)').style.backgroundColor  = 'green';;
-
-
-
 switch (diaSemana){
     case 'Mon':
         document.querySelector('.grid-item:nth-child(2)').style.backgroundColor  = '#FF3402'; //rojo
+       
+        document.querySelector('.grid-item:nth-child(8)').style.backgroundColor  = '#FF7957';
+        document.querySelector('.grid-item:nth-child(14)').style.backgroundColor  = '#FF7957';
+        document.querySelector('.grid-item:nth-child(20)').style.backgroundColor  = '#FF7957';
+        document.querySelector('.grid-item:nth-child(26)').style.backgroundColor  = '#FF7957';
+        document.querySelector('.grid-item:nth-child(32)').style.backgroundColor  = '#FF7957';
 
         if (hora >= '06:0:00' && hora <= '12:15:00') {
             document.querySelector('.grid-item:nth-child(8)').style.backgroundColor  = '#FF3402';
@@ -28,6 +30,12 @@ switch (diaSemana){
     case 'Tue':
         document.querySelector('.grid-item:nth-child(3)').style.backgroundColor  = '#FF3402';
 
+        document.querySelector('.grid-item:nth-child(9)').style.backgroundColor  = '#FF7957';
+        document.querySelector('.grid-item:nth-child(15)').style.backgroundColor  = '#FF7957';
+        document.querySelector('.grid-item:nth-child(21)').style.backgroundColor  = '#FF7957';
+        document.querySelector('.grid-item:nth-child(27)').style.backgroundColor  = '#FF7957';
+        document.querySelector('.grid-item:nth-child(33)').style.backgroundColor  = '#FF7957';
+
         if (hora >= '06:0:00' && hora < '19:0:00') {
             document.querySelector('.grid-item:nth-child(15)').style.backgroundColor  = '#FF3402';
             document.querySelector('.grid-item:nth-child(13)').style.backgroundColor  = '#FF3402';           
@@ -38,8 +46,14 @@ switch (diaSemana){
     break;
 
     case 'Wed':
-    document.querySelector('.grid-item:nth-child(4)').style.backgroundColor  = '#FF3402';
+        document.querySelector('.grid-item:nth-child(4)').style.backgroundColor  = '#FF3402';
 
+        document.querySelector('.grid-item:nth-child(10)').style.backgroundColor  = '#FF7957';
+        document.querySelector('.grid-item:nth-child(16)').style.backgroundColor  = '#FF7957';
+        document.querySelector('.grid-item:nth-child(22)').style.backgroundColor  = '#FF7957';
+        document.querySelector('.grid-item:nth-child(28)').style.backgroundColor  = '#FF7957';
+        document.querySelector('.grid-item:nth-child(34)').style.backgroundColor  = '#FF7957';
+    
         if (hora >= '06:0:00' && hora <= '17:0:00') {
             document.querySelector('.grid-item:nth-child(22)').style.backgroundColor  = '#FF3402';
             document.querySelector('.grid-item:nth-child(19)').style.backgroundColor  = '#FF3402';           
@@ -52,6 +66,12 @@ switch (diaSemana){
     case 'Thu':
         document.querySelector('.grid-item:nth-child(5)').style.backgroundColor  = '#FF3402';
 
+        document.querySelector('.grid-item:nth-child(11)').style.backgroundColor  = '#FF7957';
+        document.querySelector('.grid-item:nth-child(17)').style.backgroundColor  = '#FF7957';
+        document.querySelector('.grid-item:nth-child(23)').style.backgroundColor  = '#FF7957';
+        document.querySelector('.grid-item:nth-child(29)').style.backgroundColor  = '#FF7957';
+        document.querySelector('.grid-item:nth-child(35)').style.backgroundColor  = '#FF7957';
+
         if (hora >= '06:0:00' && hora <= '12:15:00') {
             document.querySelector('.grid-item:nth-child(11)').style.backgroundColor  = '#FF3402';
             document.querySelector('.grid-item:nth-child(7)').style.backgroundColor  = '#FF3402';           
@@ -63,6 +83,12 @@ switch (diaSemana){
 
     case 'Fri':
         document.querySelector('.grid-item:nth-child(6)').style.backgroundColor  = '#FF3402';
+      
+        document.querySelector('.grid-item:nth-child(12)').style.backgroundColor  = '#FF7957';
+        document.querySelector('.grid-item:nth-child(18)').style.backgroundColor  = '#FF7957';
+        document.querySelector('.grid-item:nth-child(24)').style.backgroundColor  = '#FF7957';
+        document.querySelector('.grid-item:nth-child(30)').style.backgroundColor  = '#FF7957';
+        document.querySelector('.grid-item:nth-child(36)').style.backgroundColor  = '#FF7957';
 
         if (hora >= '06:0:00' && hora <= '18:30:00') {
             document.querySelector('.grid-item:nth-child(24)').style.backgroundColor  = '#FF3402';
@@ -70,7 +96,3 @@ switch (diaSemana){
         }
     break;
 }
-
-
-document.write(diaSemana);
-document.write(hora);
